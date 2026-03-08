2e652db3bb13caebe2804c728b666240d90d75e5
HMT-xs <1413335905@qq.com>
2026-03-08 17:38:21 +0800

    Add files via upload
    完成3月7日Java基础与几何图形练习

---
+++ 2026-3-7-java/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    static void main(String[] args){
+        int year=2024;
+        System.out.println("Hello,World");
+        System.out.println(year);
+    }
+}
---
+++ 2026-3-7-java/Test.java
@@ -0,0 +1,16 @@
+public class Test {
+    public static void main(String[] args){
+        int a=10;
+        double b=3.5;
+        double result1=a+(int)b;
+
+        int c=7;
+        int d=2;
+        double result2=c/d;
+        double result3=(double)c/d;
+
+        System.out.println("result1:"+result1);
+        System.out.println("result2:"+result2);
+        System.out.println("result3:"+result3);
+    }
+}
